e5fce046dd5d19dc958401c18ab01657880721d2
timdavidson2 <timdavidson2@gmail.com>
2020-11-05 19:35:33 -0500

    routine save

--- index.js
+++ index.js
@@ -95,9 +95,9 @@ Reyna's feedback is missing! Use what you know to do the following:
   1. Add this feedback to Reyna's rating - "this place is chill with really cool people, great for getting work done on weekdays"
   2. log the reviews array to the console to check your work
 */
-reviews.({[7].feedback: "this place is chill with really cool people, great for getting work done on weekdays"})
-
-
+// reviews.splice(feedback: "this place is chill with really cool people, great for getting work done on weekdays");
+// console.log(reviews);
+reviews[7].feedback = "this place is chill with really cool people, great for getting work done on weekdays"
 
 
 
